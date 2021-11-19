b74765e127b56a8c6192892e310c506db3338be2
Alex Partin <25892264+adpartin@users.noreply.github.com>
2021-11-19 09:25:11 -0600

    Create files for python env
    Including bash script and environment.yml

---
+++ conda_create_tgsa_py36.sh
@@ -0,0 +1,39 @@
+#!/bin/bash --login
+
+set -e
+
+# This script was create with the help from here:
+# https://www.youtube.com/watch?v=lu2DzaqBeDg
+# https://github.com/kaust-rccl/conda-environment-examples/tree/pytorch-geometric-and-friends
+
+
+# Manually run these commands before running this sciprt
+# conda env create --file environment.yml --force
+# conda activate py36tgsa
+# Run script as follows:
+# $ ./conda_create_on_lamina.sh
+python -m pip install torch-scatter --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html
+python -m pip install torch-sparse --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html
+python -m pip install torch-cluster --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html
+python -m pip install torch-spline-conv --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html
+python -m pip install torch-geometric --no-cache-dir
+
+# My packages
+conda install -c conda-forge ipdb=0.13.9 --yes
+conda install -c conda-forge jupyterlab=3.2.0 --yes
+conda install -c conda-forge python-lsp-server=1.2.4 --yes
+
+
+# # creates the conda environment
+# PROJECT_DIR=$PWD
+# conda env create --prefix $PROJECT_DIR/env --file $PROJECT_DIR/environment.yml --force
+
+# # activate the conda env before installing PyTorch Geometric via pip
+# conda activate $PROJECT_DIR/env
+# TORCH=1.6.0
+# CUDA=cu102
+# python -m pip install torch-scatter --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+# python -m pip install torch-sparse --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+# python -m pip install torch-cluster --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+# python -m pip install torch-spline-conv --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+# python -m pip install torch-geometric --no-cache-dir
